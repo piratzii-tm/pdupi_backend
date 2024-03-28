21d830a59124a47a0dd6e2acbb5e91346ae1e9cd
Iulian Poenaru <poenaruiulian2003@gmail.com>
2024-03-28 21:13:53 +0200

    Refactor models and API structure

--- models/instructor_model.py
+++ models/instructor_model.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Boolean, String, Integer
+from sqlalchemy import String, Column, Integer
 from pydantic import BaseModel
 from database import Base
 
 
-class ClientModel(Base):
-    __tablename__ = "clients"
+class InstructorModel(Base):
+    __tablename__ = 'instructors'
 
     id = Column(Integer, primary_key=True, index=True, unique=True, autoincrement=True)
 
@@ -12,17 +12,13 @@ class ClientModel(Base):
     last_name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
     password = Column(String(100), nullable=False)
-    is_active = Column(Boolean, nullable=False)
-    created_at = Column(String(100), nullable=False)
-    updated_at = Column(String(100), nullable=False)
+    hire_date = Column(String(100), nullable=False)
 
 
-class ClientBase(BaseModel):
-
+class InstructorBase(BaseModel):
     first_name: str
     last_name: str
     email: str
     password: str
-    is_active: bool
-    created_at: str
-    updated_at: str
+    hire_date: str
+    working_gym_id: int
